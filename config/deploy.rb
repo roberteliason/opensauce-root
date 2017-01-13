3378678b0e5b26573f5e52e549afb04fe6d5d18c
Robert Eliason <robert.eliason@theshipyard.se>
2017-01-13 14:03:42 +0100

    Verbose cap feedback

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :ssh_options, { :forward_agent => true, }
 set :deploy_via, :remote_cache
 set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
 
-set :composer_install_flags, '--no-dev --prefer-dist --no-scripts --quiet --optimize-autoloader'
+set :composer_install_flags, '--no-dev --prefer-dist --no-scripts --optimize-autoloader'
 set :composer_roles, :all
 
 namespace :deploy do
